e41b186ca351c08d31e35965025d5e833f32bc31
Justin Holmes <justinholmes@users.noreply.github.com>
2014-10-07 17:01:37 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,3 +12,5 @@ DOCKER_IMAGE="${POM_ARTIFACTID}:${BUILD_NUMBER}"
 
 sudo docker build -t "${DOCKER_IMAGE}" .
 sudo docker tag "${DOCKER_IMAGE}" "${POM_ARTIFACTID}"/latest
+sudo docker tag "${DOCKER_IMAGE}" "justinholmes/${POM_ARTIFACTID}"
+sudo docker push justinholmes/${POM_ARTIFACTID}
