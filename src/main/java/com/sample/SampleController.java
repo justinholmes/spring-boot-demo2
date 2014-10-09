611d6a98ee0039f62aacb6aeef8e457c89ba3c3f
Justin Holmes <justin.holmes@opencredo.com>
2014-10-09 11:13:41 +0100

    Added host

--- src/main/java/com/sample/SampleController.java
+++ src/main/java/com/sample/SampleController.java
@@ -6,14 +6,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+
 @Controller
 @EnableAutoConfiguration
 public class SampleController {
 
     @RequestMapping("/")
     @ResponseBody
-    String home() {
-        return "Hello World from spring boot demo 2";
+    String home() throws UnknownHostException {
+        return "Hello World from spring boot demo 2: " + Inet4Address.getLocalHost().getHostAddress() + ":" +Inet4Address.getLocalHost().getHostName();
     }
 
     public static void main(String[] args) throws Exception {
